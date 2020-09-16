6364040e7a6e82d97a4259b4e3e56e2d70a84345
bigcat0815 <bigcat0815@gmail.com>
2020-09-17 03:45:51 +0900

    #state1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import React, { Component } from 'react';
 import TOC from './Components/TOC';
 import Subject from './Components/Subject';
 import Contents from './Components/Content';
+import Info from './Components/Info';
 import './App.css';
 //import ContainerExample from './Components/ContainerExampleAlignment'
 
 import { Container, Divider } from 'semantic-ui-react'
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      subject:{title:'WEB',sub:'World Wide Web'},
+      
+      contents:[
+        {id:1 , title : 'HTML',desc :"HTML is for information"},
+        {id:2 , title : 'CSS',desc :"CSS is for information"},
+        {id:3 , title : 'Java',desc :"Java is for information"}
+      ],
+      
+      s_items:[
+        {id:1, name : 'Apples', price : '1200'},
+        {id:2, name : 'Pears', price : '2700'},
+        {id:3, name : 'Oranges', price : '300'}
+      ]
+    }
+  }
   render(){
     return(
       <div className = "App">         
           
-              <Subject title="Web" sub ="World Wide Web"></Subject>            
-              <Subject title="React" sub ="React is Veryeasy WebApp!"></Subject>                            
+          <Subject 
+              title={this.state.subject.title} 
+              sub ={this.state.subject.sub}></Subject>            
+
           <Container textAlign='justified'>   
           <Divider/>                  
-            <p><TOC></TOC></p>
-            <p><Contents title ="HTML" desc="HTML is HyerperText Markup Language."></Contents></p>
-          </Container>       
+            <p><TOC data ={this.state.contents}></TOC></p>
+            <p><Contents title ="HTML" desc="HTML is HyerperText Markup Language."></Contents></p>           
+          </Container> 
+          <Container textAlign='justified'> 
+          <Divider/>
+            <p><Info p_item={this.state.s_items}></Info></p>
+          </Container>
       </div>
     );    
   }
---
+++ src/Components/Info.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { List } from 'semantic-ui-react'
+
+class Info extends Component
+{
+    render(){
+        var lists = [];
+        var data = this.props.p_item;
+        var i = 0;
+
+        while(i < data.length){
+            lists.push(<li key={data[i].id}><a href ={"/info/"+data[i].id}>{data[i].name}</a> 
+            <b> : {data[i].price}</b></li>);
+            ++i;        
+         }
+    return(
+        <nav>
+            <List>
+                <List.Item>{lists}</List.Item>
+            </List>        
+        </nav>
+        );
+    }
+}
+export default Info;

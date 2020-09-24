ab6da2c60acbfa8d5b00f38c4afbb1a0750c0622
bigcat0815 <bigcat0815@gmail.com>
2020-09-25 06:31:12 +0900

    #컴포넌트만들기3

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component{
     super(props);
     this.state ={
       mode:"read",
+      selected_contend_id:2,
       subject:{title:'WEB',sub:'World Wide Web'},
       welcome:{title:'Welcome',desc:"Hello, made by React"},
       contents:[
@@ -43,8 +44,16 @@ class App extends Component{
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     }else if(this.state.mode === "read"){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_contend_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i= i+1;
+      }
     }
 
     return(
@@ -59,9 +68,10 @@ class App extends Component{
   
           <Container textAlign='justified'>   
           <Divider/>                  
-           <TOC onChangePage={function(){
-      
-             this.setState({mode:'read'});
+           <TOC onChangePage={function(id){           
+            
+            this.setState({mode:'read',selected_contend_id:Number(id)});
+             
            }.bind(this)} data ={this.state.contents}></TOC>
             <Contents title ={_title} desc={_desc}></Contents>
           </Container> 
